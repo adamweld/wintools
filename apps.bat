c8b6ef8e49b5f72624cf795b570d57748f7cdbde
Adam Weld <adam.weld@gmail.com>
2021-08-24 13:31:55 -0700

    changed -a option to -y

--- apps.bat
+++ apps.bat
@@ -1,19 +1,19 @@
 @echo off
 
 :: Install git
-choco install git.install --params "/NoGuiHereIntegration /WindowsTerminal" -a
+choco install git.install --params "/NoGuiHereIntegration /WindowsTerminal" -y
 
 :: utilities
-choco install microsoft-windows-terminal 7zip putty.install mobaxterm everything k-litecodecpackfull insync windirstat handbrake rufus -a
+choco install googlechrome microsoft-windows-terminal 7zip putty.install mobaxterm everything k-litecodecpackfull insync windirstat hwinfo handbrake rufus -y
 
 :: languages
-choco install python -a
+choco install python -y
 
 :: IDE etc
-choco install vscode vcredist140 wsl -a 
+choco install vscode vcredist140 wsl -y 
 
 :: Ubuntu
 choco install wsl-ubuntu-2004 -y
 
 :: niceties
-choco install vlc spotify discord slack steam-client adobereader workflowy -a
+choco install vlc spotify discord slack steam-client adobereader workflowy -y
